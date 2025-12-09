9ba3dcd8be2b3487382f241fcb6864ffd555d57d
Lighthit <poomipatsubinmit@gmail.com>
2025-12-09 15:02:47 +0700

    test CI/CD

--- frontend/src/pages/RootPages.jsx
+++ frontend/src/pages/RootPages.jsx
@@ -5,7 +5,7 @@ export default function Root(){
         <>
         <div className="flex justify-center items-center min-h-screen">
             <h1 className="text-4xl font-bold text-red-600">
-                Welcome to 5G
+                Welcome to 5G test CI/CD V.1
             </h1>
         </div>
         </>
