b32fd17a876271af7cb93d0ba263d9f883a9c484
Lighthit <poomipatsubinmit@gmail.com>
2025-12-12 14:30:50 +0700

    adding end point for check done watching vdo

--- backend/Routes/registeration.js
+++ backend/Routes/registeration.js
@@ -77,13 +77,43 @@ registeration.get("/getRegist_Info/Check_vdo", async (req, res) => {
             "SELECT file_name FROM Registeration WHERE id = ?",
             [id]
     );
+    let [IsSuccess] = await sql.query(
+            "SELECT success FROM Registeration WHERE id = ?",
+            [id]
+    );
     if (rows.length > 0) {
         let url = await getSignedVideoURL(rows[0].file_name+".mp4")
         //console.log(url);
-        return res.status(200).json(url);
+        return res.status(200).json({
+             urls : url ,
+             IsSuccess : IsSuccess[0].success
+        });
     }else{
         return res.status(500).json({status : "not found"});
     }
+});
+
+
+// POST /Check Database
+registeration.post("/getRegist_Info/Check_vdo/isDone", async (req, res) => {
+  try {
+    const { id, Issuccess } = req.query;   // <<<<< เปลี่ยนเป็น query
+
+    const [rows] = await sql.query(
+      "UPDATE Registeration SET success = ? WHERE id = ?",
+      [Issuccess, id]
+    );
+
+    return res.json({
+      success: true,
+      message: "Update success status completed",
+      affectedRows: rows.affectedRows
+    });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ success: false, error: err });
+  }
 });
 
 
